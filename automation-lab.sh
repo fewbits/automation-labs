1d206540e651b60a9ad7dcf8cc0119c62973a7fa
Eric Guimaraes de Sousa Silva <eric.gssilva@gmail.com>
2017-08-06 11:09:19 -0300

    Updating repository

--- automation-lab.sh
+++ automation-lab.sh
@@ -39,18 +39,8 @@ function labStart() {
     printLog error "Wrong syntax. Try `basename $0` --help"
   fi
 
-  #printLog info "Correcting permissions of SSH files"
-  #sleep 1
-  #chmod 700 docker/rundeck/volumes/rundeck-ssh
-  #chmod 600 docker/rundeck/volumes/rundeck-ssh/id_rsa
-  #chmod 644 docker/rundeck/volumes/rundeck-ssh/id_rsa.pub
-  #chmod 700 docker/ansible/files/home-ssh
-  #chmod 600 docker/ansible/files/home-ssh/id_rsa
-  #chmod 644 docker/ansible/files/home-ssh/id_rsa.pub
-  #chmod 640 docker/ansible/files/home-ssh/authorized_keys
-
   printLog info "Building the Automation Lab..."
-  docker-compose --file ${sysDockerComposePath} build #2>/dev/null
+  docker-compose --file ${sysDockerComposePath} build 2>/dev/null
 
   printLog info "Starting the Automation Lab..."
   docker-compose --file ${sysDockerComposePath} up -d #2>/dev/null
@@ -68,11 +58,12 @@ function labStart() {
   ## Configuring Rundeck
 
   # Waiting for Rundeck to start
-  #waitForApp "rundeck" '${RD_URL}'
+  waitForApp "rundeck" '${RD_URL}'
 
   # Importing Rundeck Jobs
-  #rundeckJobLoad "ansible-lab" "rundeck-update.yml" "yaml" "update"
-  #rundeckJobLoad "ansible-lab" "rundeck-playbook.yml" "yaml" "update"
+  rundeckJobLoad "ansible-lab" "rundeck-update.yml" "yaml" "update"
+  rundeckJobLoad "ansible-lab" "rundeck-playbook.yml" "yaml" "update"
+  rundeckJobLoad "ansible-lab" "rundeck-hello-world.yml" "yaml" "update"
 
   ## Configuring GitLab
 
@@ -89,7 +80,7 @@ function labStart() {
   #docker-compose --file ${sysDockerComposePath} exec gitlab bash -c '/etc/gitlab/gitlab-config.sh' 2> /dev/null
 
   # OK - We are all set
-  #printLog info "The lab is ready to use - Go to http://${usrExternalIP}:8080 and have fun :)"
+  printLog info "The lab is ready to use - Go to http://${usrExternalIP}:8080 and have fun :)"
 }
 
 function labStop() {
